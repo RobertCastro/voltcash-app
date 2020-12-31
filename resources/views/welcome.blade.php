692f1347f28a6248fcf7b3f47528e2da29af1363
Robert Castro <oneroberth@gmail.com>
2020-12-30 20:48:27 -0500

    Prueba jenkins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Voltcash</h1>
+                    <h1 class="text-gray-100 border-gray-50">Voltcash</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
